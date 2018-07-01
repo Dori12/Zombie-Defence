3140d31179da0b9178bb98d5e9d551c408c93a06
DESKTOP-H8632P8\ksm <sunlight1994@imrlab.hoseo.edu>
2018-07-01 15:54:41 +0900

    애셋 추가

---
+++ Assets/01. Scripts/01. MainScene/MainUIButton.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainUIButton : MonoBehaviour {
+
+    [SerializeField] private GameObject StartOptionUI;
+    [SerializeField] private GameObject SettingOptionUI;
+
+    bool isClickQuit;
+
+    private void Start()
+    {
+        isClickQuit = false;
+    }
+
+    public void Click_StartButton()
+    {
+        StartOptionUI.SetActive(true);
+        SettingOptionUI.SetActive(false);
+    }
+    public void Click_SettingButton()
+    {
+        StartOptionUI.SetActive(false);
+        SettingOptionUI.SetActive(true);
+    }
+    public void Click_QuitButton()
+    {
+        if(isClickQuit == false)
+        {
+            isClickQuit = true;
+            Invoke("GameQuit", 0.5f);
+        }
+    }
+    public void Click_SinglePlayButton()
+    {
+        SceneManager.LoadScene(1);
+    }
+    void GameQuit()
+    {
+        Application.Quit();
+    }
+}
